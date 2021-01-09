0065ff692999d64091af054f4fe8a9d2e89ea5a3
Andrew <andrew.weinmann@gmail.com>
2021-01-09 17:34:47 -0600

    Fix bug blocking range/speed research for 1.0

--- data-updates.lua
+++ data-updates.lua
@@ -1,7 +1,10 @@
 local Recipe = require('__stdlib__/stdlib/data/recipe')
+local Technology = require('__stdlib__/stdlib/data/technology')
 
 if settings.startup["nanobots-start-unlocked"].value then
     data.raw.technology["nanobots"].hidden = true
+    Technology('nano-speed-1'):remove_prereq('nanobots')
+    Technology('nano-range-1'):remove_prereq('nanobots')
     Recipe('gun-nano-emitter'):set_enabled(true)
     Recipe('ammo-nano-constructors'):set_enabled(true)
     Recipe('ammo-nano-termites'):set_enabled(true)
