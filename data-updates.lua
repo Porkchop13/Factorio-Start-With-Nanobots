de17d3fc8381686f1763763c6b8dd7e90446eb7e
Andrew <andrew.weinmann@gmail.com>
2021-06-02 19:32:35 -0500

    Version 1.1.2
    Remove cliff explosive pre-req correctly

--- data-updates.lua
+++ data-updates.lua
@@ -5,6 +5,7 @@ if settings.startup["nanobots-start-unlocked"].value then
     data.raw.technology["nanobots"].hidden = true
     Technology('nano-speed-1'):remove_prereq('nanobots')
     Technology('nano-range-1'):remove_prereq('nanobots')
+    Technology('nanobots-cliff'):remove_prereq('nanobots')
     Recipe('gun-nano-emitter'):set_enabled(true)
     Recipe('ammo-nano-constructors'):set_enabled(true)
     Recipe('ammo-nano-termites'):set_enabled(true)
